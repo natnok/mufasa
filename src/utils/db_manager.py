f34ae9e94ecb6656e7cbed8fc73e99e9bf13b40d
konleb <konleb@gmail.com>
2026-09-04 14:41:40 +0300

    Add asynchronous database manager

---
+++ src/utils/db_manager.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.repositories.hotels import HotelsRepository
+from src.repositories.rooms import RoomsRepository
+
+
+class DBManager:
+    def __init__(self, session_factory):
+        self.session_factory = session_factory
+
+    async def __aenter__(self):
+        self.session: AsyncSession = self.session_factory()
+
+        self.hotels = HotelsRepository(self.session)
+        self.room = RoomsRepository(self.session)
+
+        return self
+
+    async def __aexit__(self, *args):
+        await self.session.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
