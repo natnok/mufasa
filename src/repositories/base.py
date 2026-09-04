feaa7f1a0362d6178aaa81a79e7f8d678dab2a2c
konleb <konleb@gmail.com>
2026-09-04 15:30:22 +0300

    Implement base repository CRUD operations

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from sqlalchemy import delete, insert, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import Base
@@ -13,19 +14,52 @@ class BaseRepository:
     session: AsyncSession
 
     async def get_all(self):
-        pass
+        query = select(self.model)
+        result = await self.session.execute(query)
+        print(query.compile(compile_kwargs={"kiteral_binds": True}))
+        return [self.schema.model_validate(model) for model in result.scalars().all()]
 
-    async def get_one_or_none(self):
-        pass
+    async def get_one_or_none(self, **filter_by):
+        query = select(self.model).filter_by(**filter_by)
+        result = await self.session.execute(query)
+        print(query.compile(compile_kwargs={"kiteral_binds": True}))
+        model = result.scalars().one_or_none()
+        return self.schema.model_validate(model)
 
-    async def post(self):
-        pass
+    async def post(self, data: BaseModel):
+        stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+        result = await self.session.execute(stmt)
+        print(stmt.compile(compile_kwargs={"kiteral_binds": True}))
+        model = result.scalars().one()
+        return self.schema.model_validate(model)
 
-    async def put(self):
-        pass
+    async def put(self, data: BaseModel, exclude_unset: bool = False, **filter_by):
+        stmt = (
+            update(self.model)
+            .filter_by(**filter_by)
+            .values(**data.model_dump(exclude_unset=exclude_unset))
+            .returning(self.model)
+        )
+        result = await self.session.execute(stmt)
+        print(stmt.compile(compile_kwargs={"kiteral_binds": True}))
+        model = result.scalars().one()
+        return self.schema.model_validate(model)
 
-    async def patch(self):
-        pass
+    async def patch(self, data: BaseModel, exclude_unset: bool = True, **filter_by):
+        stmt = (
+            update(self.model)
+            .filter_by(**filter_by)
+            .values(**data.model_dump(exclude_unset=exclude_unset))
+            .returning(self.model)
+        )
+        result = await self.session.execute(stmt)
+        print(stmt.compile(compile_kwargs={"kiteral_binds": True}))
+        model = result.scalars().one()
+        return self.schema.model_validate(model)
 
-    async def delete(self):
-        pass
+    async def delete(self, **filter_by):
+        query = delete(self.model).filter_by(**filter_by).returning(self.model)
+        result = await self.session.execute(query)
+        print(query.compile(compile_kwargs={"kiteral_binds": True}))
+        model = result.scalars().one()
+        return self.schema.model_validate(model)
