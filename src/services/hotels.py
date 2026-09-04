88f9ca228976527e5b2dd6477cfac66f0577ad3f
konleb <konleb@gmail.com>
2026-09-04 14:44:31 +0300

    Add base service and hotels/rooms services
    Establishes a `BaseService` class wrapping a `DBManager`, plus initial
    `HotelsService` and `RoomsService` skeletons with placeholder CRUD
    methods (get_all, get_one_or_none, post, put, patch, delete) to be
    implemented later.

---
+++ src/services/hotels.py
@@ -0,0 +1,21 @@
+from src.services.base import BaseService
+
+
+class HotelsService(BaseService):
+    async def get_all(self):
+        pass
+
+    async def get_one_or_none(self):
+        pass
+
+    async def post(self):
+        pass
+
+    async def put(self):
+        pass
+
+    async def patch(self):
+        pass
+
+    async def delete(self):
+        pass
