1fdaccf68948703c93af6928ef3620232997a19a
konleb <konleb@gmail.com>
2026-09-04 15:39:10 +0300

    Implement hotels and rooms services

--- src/services/hotels.py
+++ src/services/hotels.py
@@ -1,21 +1,25 @@
+from src.schemas.hotels import HotelsPatch, HotelsPost
 from src.services.base import BaseService
 
 
 class HotelsService(BaseService):
     async def get_all(self):
-        pass
+        return self.db.hotels.get_all()
 
-    async def get_one_or_none(self):
-        pass
+    async def get_one_or_none(self, hotel_id: int):
+        return self.db.hotels.get_one_or_none(hotel_id=hotel_id)
 
-    async def post(self):
-        pass
+    async def post(self, data: HotelsPost):
+        hotel = self.db.hotels.post(data=data)
+        return hotel
 
-    async def put(self):
-        pass
+    async def put(self, data: HotelsPost, hotel_id: int):
+        hotel = self.db.hotels.put(data=data, hotel_id=hotel_id)
+        return hotel
 
-    async def patch(self):
-        pass
+    async def patch(self, data: HotelsPatch, hotel_id: int, exclude_unset: bool = True):
+        hotel = self.db.hotels.patch(data=data, exclude_unset=exclude_unset, hotel_id=hotel_id)
+        return hotel
 
-    async def delete(self):
-        pass
+    async def delete(self, hotel_id: int):
+        return self.db.hotels.delete(hotel_id=hotel_id)
--- src/services/rooms.py
+++ src/services/rooms.py
@@ -1,21 +1,25 @@
+from src.schemas.rooms import RoomsPatch, RoomsPost
 from src.services.base import BaseService
 
 
 class RoomsService(BaseService):
     async def get_all(self):
-        pass
+        return self.db.rooms.get_all()
 
-    async def get_one_or_none(self):
-        pass
+    async def get_one_or_none(self, hotel_id: int):
+        return self.db.rooms.get_one_or_none(hotel_id=hotel_id)
 
-    async def post(self):
-        pass
+    async def post(self, data: RoomsPost):
+        hotel = self.db.rooms.post(data=data)
+        return hotel
 
-    async def put(self):
-        pass
+    async def put(self, data: RoomsPost, hotel_id: int):
+        hotel = self.db.rooms.put(data=data, hotel_id=hotel_id)
+        return hotel
 
-    async def patch(self):
-        pass
+    async def patch(self, data: RoomsPatch, hotel_id: int, exclude_unset: bool = True):
+        hotel = self.db.rooms.patch(data=data, exclude_unset=exclude_unset, hotel_id=hotel_id)
+        return hotel
 
-    async def delete(self):
-        pass
+    async def delete(self, hotel_id: int):
+        return self.db.rooms.delete(hotel_id=hotel_id)
