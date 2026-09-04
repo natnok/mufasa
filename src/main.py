99be8b26ae4abc21d3d46c6d91189ee9a514fdda
konleb <konleb@gmail.com>
2026-09-04 14:43:52 +0300

    Register v1 API router in FastAPI app

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
+from src.api.router import api_router_v1
 from src.config import settings
 
 app = FastAPI(
     title=settings.app_title,
     version=settings.app_version,
 )
+
+app.include_router(api_router_v1)
 
 if __name__ == "__main__":
     uvicorn.run(
