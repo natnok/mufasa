d01a33e3ce17665a5239f511ed06854eac0b48bc
konleb <konleb@gmail.com>
2026-09-04 17:31:38 +0300

    Implement hotels and rooms API endpoints

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,33 +1,44 @@
 from fastapi import APIRouter
 
+from src.dependencies import DBDep
+from src.schemas.hotels import HotelsPatch, HotelsPost, HotelsResponse
+from src.schemas.responce import ApiResponse
+from src.services.hotels import HotelsService
+
 router = APIRouter()
 
 
-@router.get("")
-async def get_all():
-    pass
+@router.get("", response_model=ApiResponse[list[HotelsResponse]])
+async def get_all(db: DBDep):
+    hotel = await HotelsService(db).get_all()
+    return ApiResponse(data=hotel)
 
 
-@router.get("/{hotel_id}")
-async def get_one_or_none():
-    pass
+@router.get("/{hotel_id}", response_model=ApiResponse[HotelsResponse])
+async def get_one_or_none(db: DBDep, hotel_id: int):
+    hotel = await HotelsService(db).get_one_or_none(hotel_id=hotel_id)
+    return ApiResponse(data=hotel)
 
 
-@router.post("")
-async def post():
-    pass
+@router.post("", response_model=ApiResponse[HotelsResponse])
+async def post(db: DBDep, data: HotelsPost):
+    hotel = await HotelsService(db).post(data=data)
+    return ApiResponse(data=hotel)
 
 
-@router.put("/{hotel_id}")
-async def put():
-    pass
+@router.put("/{hotel_id}", response_model=ApiResponse[HotelsResponse])
+async def put(db: DBDep, data: HotelsPost, hotel_id: int):
+    hotel = await HotelsService(db).put(data=data, hotel_id=hotel_id)
+    return ApiResponse(data=hotel)
 
 
-@router.patch("/{hotel_id}")
-async def patch():
-    pass
+@router.patch("/{hotel_id}", response_model=ApiResponse[HotelsResponse])
+async def patch(db: DBDep, data: HotelsPatch, hotel_id: int):
+    hotel = await HotelsService(db).patch(data=data, hotel_id=hotel_id)
+    return ApiResponse(data=hotel)
 
 
-@router.delete("/{hotel_id}")
-async def delete():
-    pass
+@router.delete("/{hotel_id}", response_model=ApiResponse[HotelsResponse])
+async def delete(db: DBDep, hotel_id: int):
+    hotel = await HotelsService(db).delete(hotel_id=hotel_id)
+    return ApiResponse(data=hotel)
--- src/api/rooms.py
+++ src/api/rooms.py
@@ -1,33 +1,44 @@
 from fastapi import APIRouter
 
+from src.dependencies import DBDep
+from src.schemas.responce import ApiResponse
+from src.schemas.rooms import RoomsPatch, RoomsPost, RoomsResponse
+from src.services.rooms import RoomsService
+
 router = APIRouter()
 
 
-@router.get("")
-async def get_all():
-    pass
+@router.get("", response_model=ApiResponse[list[RoomsResponse]])
+async def get_all(db: DBDep):
+    room = await RoomsService(db).get_all()
+    return ApiResponse(data=room)
 
 
-@router.get("/{room_id}")
-async def get_one_or_none():
-    pass
+@router.get("/{room_id}", response_model=ApiResponse[RoomsResponse])
+async def get_one_or_none(db: DBDep, room_id: int):
+    room = await RoomsService(db).get_one_or_none(room_id=room_id)
+    return ApiResponse(data=room)
 
 
-@router.post("")
-async def post():
-    pass
+@router.post("", response_model=ApiResponse[RoomsResponse])
+async def post(db: DBDep, data: RoomsPost):
+    room = await RoomsService(db).post(data=data)
+    return ApiResponse(data=room)
 
 
-@router.put("/{room_id}")
-async def put():
-    pass
+@router.put("/{room_id}", response_model=ApiResponse[RoomsResponse])
+async def put(db: DBDep, data: RoomsPost, room_id: int):
+    room = await RoomsService(db).put(data=data, room_id=room_id)
+    return ApiResponse(data=room)
 
 
-@router.patch("/{room_id}")
-async def patch():
-    pass
+@router.patch("/{room_id}", response_model=ApiResponse[RoomsResponse])
+async def patch(db: DBDep, data: RoomsPatch, room_id: int):
+    room = await RoomsService(db).patch(data=data, room_id=room_id)
+    return ApiResponse(data=room)
 
 
-@router.delete("/{room_id}")
-async def delete():
-    pass
+@router.delete("/{room_id}", response_model=ApiResponse[RoomsResponse])
+async def delete(db: DBDep, room_id: int):
+    room = await RoomsService(db).delete(room_id=room_id)
+    return ApiResponse(data=room)
