6f5d3263a34289a6df03bbbdbbba99ebd8b41f3d
konleb <konleb@mail.com>
2026-09-06 13:13:57 +0300

    Add Postgres config and computed DB URL

--- src/config.py
+++ src/config.py
@@ -1,7 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic import PostgresDsn, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env.prod")
+
     app_title: str = "mufasa"
     app_version: str = "v3.02v"
 
@@ -9,5 +12,23 @@ class Settings(BaseSettings):
     app_port: int = 8000
     app_reload: bool = True
 
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
 
-settings = Settings()
+    @computed_field
+    @property
+    def DB_URL(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+
+
+settings = Settings()  # type: ignore
