1e0ab6c1af42a7782af9b1c1d6c9ac6468e2b020
konleb <konleb@mail.com>
2026-09-06 13:28:44 +0300

    Fix typos and clean up imports in database.py

--- src/database.py
+++ src/database.py
@@ -1,15 +1,13 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
-from sqlalchemy import
-
 
 engine = create_async_engine(settings.DB_URL.unicode_string())
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-engine_null_pool = create_async_engine(settings.DB_URL.unicode_string(), pollclass=NullPool)
+engine_null_pool = create_async_engine(settings.DB_URL.unicode_string(), poolclass=NullPool)
 async_session_maker_null_pool = async_sessionmaker(engine_null_pool, expire_on_commit=False)
 
 
